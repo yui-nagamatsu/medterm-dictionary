526a4548f0eacd5400cf9e8e8f41c9598ccc004a
yui-nagamatsu <yuinm129@gmail.com>
2022-03-25 17:29:17 +0900

    Give authority to waut-for-db.sh and add COMMAND about waiting for db to docker-compose.yml

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS mysite CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS 'mysiteuser'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON mysite.* TO 'mysiteuser'@'%';
+GRANT ALL PRIVILEGES ON test_database.* TO 'mysiteuser'@'%';
 
 FLUSH PRIVILEGES;

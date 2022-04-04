cd1fae36672c9bdd871c9633e867bd8fa4ff1a19
yui-nagamatsu <yuinm129@gmail.com>
2022-04-04 11:32:51 +0900

    Create model Medterm

--- app/src/backend/models.py
+++ app/src/backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Medterm(models.Model):
+    term = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    japanese = models.CharField(max_length=50)
+    def __str__(self):
+        return self.term

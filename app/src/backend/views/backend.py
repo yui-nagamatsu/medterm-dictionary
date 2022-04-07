bb7818994032d92dc5838f26503e3cc82cd46118
yui-nagamatsu <yuinm129@gmail.com>
2022-04-07 22:10:37 +0900

    Add url to word of the day

--- app/src/backend/views/backend.py
+++ app/src/backend/views/backend.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView
+from re import template
+from django.views.generic import ListView, DetailView
 from backend.models import Medterm
+from django.http import HttpResponse
+from django.views import generic
+import string
 
 class IndexView(ListView):
     template_name = 'backend/index.html'
@@ -8,6 +12,19 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs)
         random_fetch = Medterm.objects.order_by('?')[:1]
         context['word_of_the_day'] = list(random_fetch)
-        # context['alphabet_list'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1-9']
+        context['alphabet_list'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1-9']
         return context
 
+class SearchView(ListView):
+    template_name = 'backend/index.html'
+    def get_queryset(self):
+        q_word = self.request.GET.get('query')
+        if q_word:
+            term_list = Medterm.objects.filter(term__icontains=q_word)
+        else:
+            term_list = Medterm.objects.all()
+        return term_list
+
+class TermDetail(generic.DetailView):
+    model = Medterm
+    template_name = 'backend/detail.html'

40563b9c8eb33027833e7a4e3f54b5a3fe2ca29f
yui-nagamatsu <yuinm129@gmail.com>
2022-04-08 12:35:09 +0900

    Add url patterns of detail, browse and search_result

--- app/src/backend/urls.py
+++ app/src/backend/urls.py
@@ -5,5 +5,7 @@ from .views import backend
 app_name = 'backend'
 urlpatterns = [
     path('', backend.IndexView.as_view(), name = 'index'),
-    path('detail/<int:pk>/', backend.TermDetail.as_view(), name='detail')
+    path('detail/<int:pk>/', backend.TermDetail.as_view(), name='detail'),
+    path('search_result/', backend.SearchResultView.as_view(), name='search_result'),
+    path('browse/<slug:slug>', backend.BrowseView.as_view(), name='browse')
 ]
